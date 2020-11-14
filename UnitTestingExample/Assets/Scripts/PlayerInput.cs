fcf010c12a239456a404ac3e4030f73ca9a9484c
Bruno Gomes <bruno.gomes.771@outlook.com>
2020-11-14 22:02:43 +0000

    Unity Project

---
+++ UnitTestingExample/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    Vector2 movement;
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        switch (movement.x)
+        {
+            case 1f when (movement.y == 0):
+                MoveRight();
+                break;
+            case -1f when (movement.y == 0):
+                MoveLeft();
+                break;
+            case 0 when (movement.y == 1f):
+                MoveUp();
+                break;
+            case 0 when (movement.y == -1f):
+                MoveDown();
+                break;
+            case 1f when (movement.y == 1f):
+                MoveUp();
+                MoveRight();
+                break;
+            case -1f when (movement.y == 1f):
+                MoveUp();
+                MoveLeft();
+                break;
+            case 1f when (movement.y == -1f):
+                MoveDown();
+                MoveRight();
+                break;
+            case -1f when (movement.y == -1f):
+                MoveDown();
+                MoveLeft();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void MoveLeft()
+    {
+        this.transform.position += new Vector3(-1f, 0) * Time.deltaTime;
+    }
+    public void MoveRight()
+    {
+        this.transform.position += new Vector3(1f, 0) * Time.deltaTime;
+    }
+    public void MoveUp()
+    {
+        this.transform.position += new Vector3(0, 1f) * Time.deltaTime;
+    }
+    public void MoveDown()
+    {
+        this.transform.position += new Vector3(0, -1f) * Time.deltaTime;
+    }
+}
---
+++ UnitTestingExample/Assets/Tests/PlayMode/MovementTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MovementTest
+    {
+        [UnityTest]
+        public IEnumerator MovementWithEnumeratorPasses()
+        {
+            GameObject player = new GameObject();
+            PlayerInput inputs = player.AddComponent<PlayerInput>();
+
+            inputs.MoveLeft();
+
+            yield return new WaitForSeconds(1);
+
+            Assert.AreEqual(player.transform.position, new Vector3(-1, 0, 0));
+        }
+    }
+}
